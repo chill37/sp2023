6556a2a270d86a66de0cddb56951f49f662b98d0
김태우 책임/Web3 LAB <tkim@lgcns.com>
2023-06-19 01:35:24 +0900

    commit

--- src/jetty/MyClient.java
+++ src/jetty/MyClient.java
@@ -1,16 +1,51 @@
 package jetty;
 
-import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.client.api.ContentResponse;
-import org.eclipse.jetty.http.HttpMethod;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MyClient {
+	public static void main(String[] args) throws IOException {
+		// Make a request to /path1
+		String response1 = sendGetRequest("http://localhost:8080/path1");
+		System.out.println("Response from /path1: " + response1);
 
-	public static void main(String[] args) throws Exception {
-		HttpClient httpClient = new HttpClient();
-		httpClient.start();
-		ContentResponse contentRes = httpClient.newRequest("http://127.0.0.1:8081/mypath?name=guy&value=1").method(HttpMethod.GET)
-				.send();
-		System.out.println(contentRes.getContentAsString());
+		// Make a request to /path2
+		String response2 = sendGetRequest("http://localhost:8080/path2");
+		System.out.println("Response from /path2: " + response2);
+
+		// Make a request to an invalid path
+		String response3 = sendGetRequest("http://localhost:8080/invalid");
+		System.out.println("Response from /invalid: " + response3);
+	}
+
+	private static String sendGetRequest(String url) throws IOException {
+		HttpURLConnection connection = null;
+		BufferedReader reader = null;
+		StringBuilder response = new StringBuilder();
+
+		try {
+			URL requestUrl = new URL(url);
+			connection = (HttpURLConnection) requestUrl.openConnection();
+			connection.setRequestMethod("GET");
+
+			// Get the response
+			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				response.append(line);
+			}
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+			if (connection != null) {
+				connection.disconnect();
+			}
+		}
+
+		return response.toString();
 	}
 }
--- src/jetty/MyServer.java
+++ src/jetty/MyServer.java
@@ -1,7 +1,9 @@
 package jetty;
 
 import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class MyServer {
 
@@ -10,18 +12,19 @@ public class MyServer {
 	}
 
 	public void start() throws Exception {
-		Server server = new Server();
-		ServerConnector http = new ServerConnector(server);
-		http.setHost("127.0.0.1");
-		http.setPort(8081);
-		server.addConnector(http);
+		Server server = new Server(8080);
 
-		ServletHandler servletHandler = new ServletHandler();
-		servletHandler.addServletWithMapping(MyServlet.class, "/path1");
-		servletHandler.addServletWithMapping(MyServlet.class, "/path2");
+		ServletContextHandler handler = new ServletContextHandler();
+		handler.setContextPath("/");
 
-		server.setHandler(servletHandler);
+		// Create an instance of your servlet
+		MyServlet myServlet = new MyServlet();
 
+		// Map the servlet to multiple URL patterns
+		handler.addServlet(new ServletHolder(myServlet), "/path1");
+		handler.addServlet(new ServletHolder(myServlet), "/path2");
+
+		server.setHandler(handler);
 		server.start();
 		server.join();
 	}
